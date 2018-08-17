21a53bf47f09c420998828f9e6325141f1be5ef6
KRAIWIT KEYEN <kraiwit_key@cmu.ac.th>
2018-08-17 15:26:49 +0700

    ปุ่มเลข 1-9 กดแล้วออก

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,160 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+        float num1 = 0 ; 
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if(lblDisplay.Text.Length < 8 )
+            {
+                lblDisplay.Text = lblDisplay.Text + "1";
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "2";
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "3";
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "4";
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "5";
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "6";
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "7";
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "8";
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "9";
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + "0";
+            }
+        }
+
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "";
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            num1 = float.Parse(lblDisplay.Text);
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
