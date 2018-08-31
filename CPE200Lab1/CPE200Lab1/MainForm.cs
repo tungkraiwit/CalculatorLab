4fbaeddc392e18e95fb7a00b23c65e541c95db13
KRAIWIT KEYEN <kraiwit_key@cmu.ac.th>
2018-08-31 21:56:39 +0700

    ทำเปอเซ็นต์,รากที่2,1/X

--- CPE200Lab1/CPE200Lab1/MainForm.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.cs
@@ -18,6 +18,7 @@ namespace CPE200Lab1
         private bool isAfterEqual;
         private string firstOperand;
         private string operate;
+        private CalculatorEngine engine;
 
         private void resetAll()
         {
@@ -27,51 +28,15 @@ namespace CPE200Lab1
             isAfterOperater = false;
             isAfterEqual = false;
         }
-
-        private string calculate(string operate, string firstOperand, string secondOperand, int maxOutputSize = 8)
-        {
-            switch(operate)
-            {
-                case "+":
-                    return (Convert.ToDouble(firstOperand) + Convert.ToDouble(secondOperand)).ToString();
-                case "-":
-                    return (Convert.ToDouble(firstOperand) - Convert.ToDouble(secondOperand)).ToString();
-                case "X":
-                    return (Convert.ToDouble(firstOperand) * Convert.ToDouble(secondOperand)).ToString();
-                case "÷":
-                    // Not allow devide be zero
-                    if(secondOperand != "0")
-                    {
-                        double result;
-                        string[] parts;
-                        int remainLength;
-
-                        result = (Convert.ToDouble(firstOperand) / Convert.ToDouble(secondOperand));
-                        // split between integer part and fractional part
-                        parts = result.ToString().Split('.');
-                        // if integer part length is already break max output, return error
-                        if(parts[0].Length > maxOutputSize)
-                        {
-                            return "E";
-                        }
-                        // calculate remaining space for fractional part.
-                        remainLength = maxOutputSize - parts[0].Length - 1;
-                        // trim the fractional part gracefully. =
-                        return result.ToString("N" + remainLength);
-                    }
-                    break;
-                case "%":
-                    //your code here
-                    break;
-            }
-            return "E";
-        }
-
         public MainForm()
         {
             InitializeComponent();
 
             resetAll();
+            // 1. new culculatorEngine() => instate an object
+            // 2. reference to that object with engine variable
+            // LHS = RHS
+            engine = new CalculatorEngine();
         }
 
         private void btnNumber_Click(object sender, EventArgs e)
@@ -118,13 +83,19 @@ namespace CPE200Lab1
                 case "+":
                 case "-":
                 case "X":
+                case "√":
+                case "1/X":
                 case "÷":
                     firstOperand = lblDisplay.Text;
                     isAfterOperater = true;
                     break;
                 case "%":
                     // your code here
+                    string secondOperand = lblDisplay.Text;
+                    firstOperand = engine.calculate(operate, firstOperand, secondOperand);
+                    lblDisplay.Text = firstOperand;
                     break;
+                
             }
             isAllowBack = false;
         }
@@ -136,7 +107,7 @@ namespace CPE200Lab1
                 return;
             }
             string secondOperand = lblDisplay.Text;
-            string result = calculate(operate, firstOperand, secondOperand);
+            string result = engine.calculate(operate, firstOperand, secondOperand);
             if (result is "E" || result.Length > 8)
             {
                 lblDisplay.Text = "Error";
@@ -226,6 +197,36 @@ namespace CPE200Lab1
                     lblDisplay.Text = "0";
                 }
             }
+        }
+
+        private void lblDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void M_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MS_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MR_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MC_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
