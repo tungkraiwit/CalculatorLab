8ade24399b07028d9702ac13ac95e61fcf496fca
KRAIWIT KEYEN <kraiwit_key@cmu.ac.th>
2018-09-14 15:12:49 +0700

    เพิ่ม :CalculatorEngine  ใน RPNCalculatorEngine

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -15,12 +15,12 @@ namespace CPE200Lab1
         private bool isNumberPart = false;
         private bool isContainDot = false;
         private bool isSpaceAllowed = false;
-        private CalculatorEngine engine;
+        private RPNCalculatorEngine engine;
 
         public ExtendForm()
         {
             InitializeComponent();
-            engine = new CalculatorEngine();
+            engine = new RPNCalculatorEngine();
         }
 
         private bool isOperator(char ch)
@@ -37,7 +37,7 @@ namespace CPE200Lab1
 
         private void btnNumber_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text is "Error")
+                if (lblDisplay.Text is "Error")
             {
                 return;
             }
